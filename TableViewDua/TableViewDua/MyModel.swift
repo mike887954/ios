005ee338795ebca41ccef84ebdd925ed5cb89ee6
Justinus Andjarwirawan <justinus@gmail.com>
2016-03-24 14:04:52 +0700

    added

---
+++ TableViewDua/TableViewDua/MyModel.swift
@@ -0,0 +1,9 @@
+//
+//  MyModel.swift
+//  TableViewDua
+//
+//  Created by Justinus Andjarwirawan on 3/24/16.
+//  Copyright © 2016 Petra Christian University. All rights reserved.
+//
+
+import Foundation
